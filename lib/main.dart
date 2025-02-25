6948f403ae30d890ea295b14b88fc3eff151cfe0
ANGEL_MIGUEL_VALENCIA_CARRILLO <a.22308051280738@cbtis128.edu.mx>
2025-02-25 16:58:26 +0000

    navaimg

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Mis imagenes",
+            style: TextStyle(
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              color: Color(0xff000000),
+            ),
+          ),
+          backgroundColor: Color(0xff27a4c3),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Angel Valencia Mat:22308051280738 6j',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff000000),
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Angel-Miguel-Valencia-Carrillo/imagenes-para-APP-flutter-6j/refs/heads/main/bacardi1.jpg',
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'botella de bacardi',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff000000),
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Angel-Miguel-Valencia-Carrillo/imagenes-para-APP-flutter-6j/refs/heads/main/descarga1.jpg',
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'botella de bacardi 2',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff020101),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); // Material app
+  } // widgets
+} // clase mi imagen
